9b3c168227569eb36d49ab6b336848c2d4ca8bcf
GeekMasher <geekmasher@gmail.com>
2024-04-29 15:35:18 +0100

    feat: Update init and deps

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 --[[
- _____  ___    _______    ______  ___      ___  __     ___      ___ 
+ _____  ___    _______    ______  ___      ___  __     ___      ___
 (\"   \|"  \  /"     "|  /    " \|"  \    /"  ||" \   |"  \    /"  |
 |.\\   \    |(: ______) // ____  \\   \  //  / ||  |   \   \  //   |
 |: \.   \\  | \/    |  /  /    ) :)\\  \/. ./  |:  |   /\\  \/.    |
@@ -18,14 +18,14 @@ Resources:
 -- Install Lazy if not present on the system
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -34,12 +34,12 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Nerd Font
-vim.g.have_nerd_font = true 
+vim.g.have_nerd_font = true
 
 -- Encoding
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = 'utf-8'
+vim.opt.fileencoding = "utf-8"
 
 -- Numbers
 vim.opt.number = true
@@ -48,7 +48,7 @@ vim.opt.relativenumber = true
 -- Show mode
 vim.opt.showmode = false
 -- Clipboard shared
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 -- Other
 vim.opt.breakindent = true
@@ -58,12 +58,12 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.undofile = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Mouse (No Mouse)
 vim.opt.mouse = ""
 
--- Splits 
+-- Splits
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
@@ -95,24 +95,24 @@ vim.opt.wrap = false
 vim.opt.backspace = { "start", "eol", "indent" }
 
 -- search
-vim.opt.hlsearch = true 
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 -- Find files
 vim.opt.path:append({ "**" })
 -- Ignore Files
 vim.opt.wildignore:append({
-	-- Git
-	"**/.git/*",
-	-- Python
-	"**/.pyc",
-	-- Rust
-	"**/target/",
-	-- JS/TS
-	"*/node_modules/*",
-	-- Mobile
-	"**/ios/*",
-	"**/android/*",
+    -- Git
+    "**/.git/*",
+    -- Python
+    "**/.pyc",
+    -- Rust
+    "**/target/",
+    -- JS/TS
+    "*/node_modules/*",
+    -- Mobile
+    "**/ios/*",
+    "**/android/*",
 })
 
 local opts = { noremap = true, silent = true }
@@ -129,21 +129,21 @@ vim.keymap.set("n", "J", "20jzz", opts)
 vim.keymap.set("n", "K", "20kzz", opts)
 
 -- Disable Arrow Keys (you have to learn some day)
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 -- Split Navigation
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- LSP
 -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v1.x/doc/md/lsp.md#default-keybindings
 vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
 vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<cr>")
-vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
+vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
 
 vim.keymap.set("n", "<leader>k", "<cmd>lua vim.lsp.buf.hover()<cr>")
 vim.keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
@@ -155,7 +155,7 @@ vim.keymap.set("n", "<leader>gw", "<cmd>lua vim.lsp.buf.workspace_symbol()<cr>")
 vim.keymap.set("n", "<leader>o", vim.diagnostic.open_float, opts)
 
 -- TODO Expand Rust Macros
--- vim.keymap.set("n", "<leader>ge", '<cmd>vim.cmd.RustLsp("expandMacro")<cr>')
+vim.keymap.set("n", "<leader>ge", '<cmd>lua vim.cmd.RustLsp("expandMacro")<cr>')
 
 -- Lazy man's saving...
 vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>")
@@ -164,24 +164,22 @@ vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
-
 -- Lazy Plugins
 require("lazy").setup("plugins")
 
 -- On Save run formatter
 local formatters = vim.api.nvim_create_augroup("formatters", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	group = formatters,
-	callback = function()
-		vim.lsp.buf.format()
-	end,
+    group = formatters,
+    callback = function()
+        vim.lsp.buf.format()
+    end,
 })
-
 
 -- CodeQL
 local codeql = require("codeql")
 vim.keymap.set("n", "<leader>qd", function()
-	codeql.list_databases()
+    codeql.list_databases()
 end)
 
 -- Trouble
