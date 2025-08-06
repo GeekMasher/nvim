1e2ef07794766b00e0b1ec29b43c53d88fa542aa
GeekMasher <geekmasher@gmail.com>
2025-08-06 09:22:38 +0100

    feat: Remove lsp keymaps

--- init.lua
+++ init.lua
@@ -142,24 +142,6 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- LSP
--- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v1.x/doc/md/lsp.md#default-keybindings
--- vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
--- vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<cr>")
--- vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
---
--- vim.keymap.set("n", "<leader>k", "<cmd>lua vim.lsp.buf.hover()<cr>")
--- vim.keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
--- vim.keymap.set("n", "<C-K>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
---
--- vim.keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.document_symbol()<cr>")
--- vim.keymap.set("n", "<leader>gw", "<cmd>lua vim.lsp.buf.workspace_symbol()<cr>")
---
--- vim.keymap.set("n", "<leader>o", vim.diagnostic.open_float, opts)
-
--- TODO Expand Rust Macros
-vim.keymap.set("n", "<leader>ge", '<cmd>lua vim.cmd.RustLsp("expandMacro")<cr>')
-
 -- Lazy man's saving...
 vim.keymap.set("n", "qq", "<cmd>:wq<cr>")
 vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>")
